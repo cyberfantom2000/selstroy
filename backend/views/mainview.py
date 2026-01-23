8cc54944ffd8552c9cf28612195489cf8934e1e1
Daniil Evstigneev <d.evstigneev@vkteam.ru>
2026-01-23 18:29:53 +0300

    add gallery page

--- backend/views/mainview.py
+++ backend/views/mainview.py
@@ -11,6 +11,7 @@ class MainViewRouter:
         self.router.add_api_route('/', self.index)
         self.router.add_api_route('/projects', self.projects)
         self.router.add_api_route('/promo', self.promo)
+        self.router.add_api_route('/gallery', self.gallery)
 
     async def index(self, request: Request) -> HTMLResponse:
         return templates.TemplateResponse(
@@ -28,4 +29,10 @@ class MainViewRouter:
         return templates.TemplateResponse(
             request=request,
             name='pages/promo.html',
+        )
+
+    async def gallery(self, request: Request) -> HTMLResponse:
+        return templates.TemplateResponse(
+            request=request,
+            name='pages/gallery.html',
         )
